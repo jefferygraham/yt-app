48c197a365e80a892a5e18be3a11ef7c8f2c5707
Jeff Graham <jeffery.lemar.graham@gmail.com>
2021-03-29 14:51:42 -0400

    ready for deploy

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onTermSubmit }) => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    onTermSubmit(term);
+  };
+
+  return (
+    <div className='ui segment search-bar'>
+      <form className='ui form' onSubmit={onSubmit}>
+        <div className='field'>
+          <label>Search for a video:</label>
+          <input
+            type='text'
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/VideoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedVideoList = videos.map((video) => (
+    <VideoItem
+      key={video.id.videoId}
+      video={video}
+      onVideoSelect={onVideoSelect}
+    />
+  ));
+
+  return <div className='ui relaxed divided list'>{renderedVideoList}</div>;
+};
+
+export default VideoList;
